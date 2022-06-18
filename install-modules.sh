5acda2ca61b26581ec663f4519a2e2f20bbe1025
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-06-18 10:02:35 +0900

    add install script

---
+++ install-modules.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt install -y  \
+    libavcodec-dev       \
+    libavfilter-dev      \
+    libavformat-dev      \
+    libavdevice-dev      \
+    libavutil-dev        \
+    libswresample-dev    \
+    libpostproc-dev      \
+    libswscale-dev
