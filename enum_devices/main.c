ce07ea1b9b6a651607db31119e32d8650e36b8f8
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-06-19 09:17:43 +0900

    [WIP] enum input devices

---
+++ enum_devices/main.c
@@ -0,0 +1,45 @@
+/*!
+    @file
+
+    ffmpeg test program for device enum
+*/
+#include <stdio.h>
+#include <libavformat/avformat.h>
+#include <libavdevice/avdevice.h>
+
+
+static void enum_input_devices()
+{
+
+    AVInputFormat *fmt = NULL;
+    while (1)
+    {
+        fmt = av_input_audio_device_next(fmt);
+        if (fmt == NULL)
+        {
+            break;
+        }
+        printf("name = %-10s, long_name = %s\n", fmt->name, fmt->long_name);
+
+        AVDeviceInfoList* device_list = NULL;
+        int ret = avdevice_list_input_sources(
+            fmt,
+            NULL, // device_name
+            NULL, // device_options
+            &device_list
+        );
+        if (ret >= 0)
+        {
+            printf("avdevice_list_input_sources returns %d\n", ret);
+        }
+        avdevice_free_list_devices(&device_list);
+        device_list = NULL;
+    }
+}
+
+int main()
+{
+    avdevice_register_all();
+    enum_input_devices();
+    return 0;
+}
